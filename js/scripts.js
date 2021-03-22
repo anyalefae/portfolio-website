1024d046abaa8570c9990edda2acee1b6e78b9e4
Jordanya le Fae <anyalefae@gmail.com>
2021-03-22 17:53:09 -0400

    edit menu icon and function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi there! Welcome to my portfolio site. I hope you enjoy your visit.");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
