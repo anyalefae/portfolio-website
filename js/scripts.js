d3724598bde950713aa9860fdab3e3da873da5a6
Jordanya le Fae <anyalefae@gmail.com>
2021-04-13 09:06:26 -0400

    beautify and lint

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-console.log("Hi there! Welcome to my portfolio site. I hope you enjoy your visit.");
+console.log('Hi there! Welcome to my portfolio site. I hope you enjoy your visit.')
 
 function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
